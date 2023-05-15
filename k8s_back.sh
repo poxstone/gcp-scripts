db0a92e52f0bfad6a9083fdaba62127041e36eeb
Your Name <you@example.com>
2023-05-15 08:39:41 -0500

    update all scripts

---
+++ k8s_back.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define variables
+OUTPUT_DIR="./backup";
+mkdir -p ${OUTPUT_DIR};
+rm -rf ${OUTPUT_DIR}/*;
+
+# Lista todos los Resource Definitions
+RESOURCES=$(kubectl api-resources -o name);
+
+# Itera sobre los Resource Definitions y descarga la configuración YAML de cada recurso
+for RESOURCE in ${RESOURCES};do
+  if [[  "${RESOURCE}" == "bindins" ]];then
+    RESOURCE="rolebinding,clusterrolebinding";
+  fi;
+  echo "Try no namespace Resource: ${RESOURCE}";
+  kubectl get ${RESOURCE} -o yaml > "${OUTPUT_DIR}/${RESOURCE}.yaml";
+  echo "Try whit namespace Resource: ${RESOURCE}";
+  kubectl get ${RESOURCE} -o yaml --all-namespaces >> "${OUTPUT_DIR}/${RESOURCE}.yaml";    
+done;
+
+# Comprime los archivos YAML descargados en un solo archivo
+tar -czvf "${OUTPUT_DIR}.tgz" "${OUTPUT_DIR}";
